9f6d8b12b82ae067cb026a44b6014b05d030b89e
Reymundo Tenorio <reymundotenorio@gmail.com>
2024-10-17 22:57:18 -0600

    [database] setup and wait_for_db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtrack(x, y):
+    """Subtrack x from y and return result."""
+    return y - x
